3e4ecb0692305f6e8776918387ba6ba7938f4326
ConnorWhite <connor.bcw@gmail.com>
2017-02-14 20:17:41 -0600

    Misc.
    -Split SpaceExplorer.php into head, main, footer
    -Added favicon
    -Added description meta tag
    -Deleted README

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <pre id="copyright">© <?php echo date("Y"); ?> Connor White</pre>
+    <pre id="version">Version 0.1</pre>
+  </body>
+  <script type="text/javascript" src="SpaceExplorer.js"></script>
+</html>
---
+++ head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Space Explorer</title>
+		<meta description="An ASCII art based space RPG, and love letter to Space Exploration Technologies Corp." />
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+<body>
+	<pre id="intro">
+		<div>Books read: <span id="books">-</span></div>
+		<div>People talked to: <span id="talked">-</span></div>
+		<div><a id="button">Start a company (UnZip3)</a></div>
+		<div>C: $<span class="credits"></span></span>
+	</pre>
