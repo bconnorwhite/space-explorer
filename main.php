f5285beb0507b303fb549a9d4c41e5d9cc11ca8f
ConnorWhite <connor.bcw@gmail.com>
2017-02-19 21:23:34 -0600

    TODO plans, Added more keys to main.php

--- main.php
+++ main.php
@@ -33,8 +33,8 @@
 <span id="opened-hinge">}==============================================================================={
 </span></span><span id="closed-hinge">.-------------------------------------------------------------------------------.
 </span>/<span class="inside"> <a id="escape">ESC</a> <a id="dim">[&#9788;]</a> <a id="bright">[&#9728;]</a> ##### ##### ##### #### #### #### #### #### #### #### #### #### <a id="power">(|)</a> </span>\
-/<span class="inside"> #### #### #### #### #### #### #### #### ##### ##### #### #### ##### ##### ###### </span>\
-/<span class="inside"> ##### #### #### #### #### #### #### #### #### #### #### #### #### #### ##### ##### </span>\
+/<span class="inside"> #### ##1## ##2## ##3## ##4## ##5## ##6## ##7## ##8## ##9## ##0## ##### ##### DEL </span>\
+/<span class="inside"> #TAB ##Q## ##W## ##E## ##R## ##T## ##Y## ##U## ##I## ##O## ##P## ##### ##### ##### </span>\
 <span class="backlight">/                                      .'`'--'``.                                      \
 </span><span class="backlight">/                                      :.         '.                                     \
 </span><span class="backlight">/                                         :         :                                      \
@@ -42,8 +42,8 @@
 </span><span class="backlight">/                                         '..__' `__.'                                         \
 </span><span class="backlight">/                                               .,                                               \
 </span><span class="backlight">/                                              :'`                                                 \
-</span><span class="inside-swap">/ ###### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### <a id="enter">ENTER</a> \
-</span><span class="inside-swap">/ ####### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ### =^=  ### \
+</span><span class="inside-swap">/ #ALLCAPS# ##A## ##S## ##D## ##F## ##G## ##H## ##J## ##K## ##L## ##### ##:##  <a id="enter">#ENTER#</a> \
+</span><span class="inside-swap">/ SHIFT ##Z## ##X## ##C## ##V## ##B## ##N## ##M## ##### ##.## ##### SHIFT       =^=      \
 </span><span class="inside-swap">/ ##### ##### ##### ###### ################################## ###### ##### <=== ==v== ===> \
 </span><span class="inside-swap">/                                    ____________________                                    \
 </span><span class="inside-swap">/                                    /                    \                                    \
