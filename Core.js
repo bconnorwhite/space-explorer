4c61d3ecbd2e7c4b68a920754c010f0dd7351fda
ConnorWhite <connor.bcw@gmail.com>
2017-02-19 19:10:42 -0600

    Opening and closing laptop, Apple Logo

--- Core.js
+++ Core.js
@@ -5,23 +5,26 @@
  * -Uses Base.js to manage window
  */
 
-function initExplorer(exp){
-  explorer = exp;
-}
-
 //(function(){
   //Initialize Explorer
   var explorer;
   ajaxGetJSON("GetExplorer.php", initExplorer);
+  function initExplorer(exp){
+    explorer = exp;
+  }
 
-  //Initialize Window
+  //Initialize Sandbox
   var sandbox = Sandbox();
 
-  //Initialize Keyboard
+  //Initialize laptop
+  var laptop = Laptop(sandbox);
+  laptop.init();
+
+  //Initialize Screen
   var screen = Screen(sandbox);
   screen.init();
+  //Initialize Keyboard
   var keyboard = Keyboard(sandbox);
   keyboard.init();
-
 
 //})();
--- footer.php
+++ footer.php
@@ -2,8 +2,10 @@
     <pre id="version">Version 0.1</pre>
   </body>
   <script type="text/javascript" src="Sandbox.js"></script>
+  <script type="text/javascript" src="Laptop.js"></script>
   <script type="text/javascript" src="Keyboard.js"></script>
   <script type="text/javascript" src="Screen.js"></script>
   <script type="text/javascript" src="Base.js"></script>
   <script type="text/javascript" src="Core.js"></script>
 </html>
+<!-- ASCII inspiration credit: Joan G. Stark -->
