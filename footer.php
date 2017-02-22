4002deecee57a23127087be7a1a02ab3818f65c9
ConnorWhite <connor.bcw@gmail.com>
2017-02-21 23:11:30 -0600

    Footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
     <pre id="copyright">© <?php echo date("Y"); ?> Connor White</pre>
     <pre id="version">Version 0.1</pre>
   </body>
-  <script type="text/javascript" src="Sandbox.js"></script>
-  <script type="text/javascript" src="Laptop.js"></script>
-  <script type="text/javascript" src="Keyboard.js"></script>
-  <script type="text/javascript" src="Terminal.js"></script>
-  <script type="text/javascript" src="Screen.js"></script>
-  <script type="text/javascript" src="Base.js"></script>
-  <script type="text/javascript" src="Core.js"></script>
+  <script type="text/javascript" src="/scripts/Sandbox.js"></script>
+  <script type="text/javascript" src="/scripts/Laptop.js"></script>
+  <script type="text/javascript" src="/scripts/Keyboard.js"></script>
+  <script type="text/javascript" src="/scripts/Terminal.js"></script>
+  <script type="text/javascript" src="/scripts/Screen.js"></script>
+  <script type="text/javascript" src="/scripts/Base.js"></script>
+  <script type="text/javascript" src="/scripts/Core.js"></script>
 </html>
 <!-- ASCII inspiration credit: Joan G. Stark -->
