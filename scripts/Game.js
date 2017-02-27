a3d57b462bd424babd37a741de5c070afc75598a
ConnorWhite <connor.bcw@gmail.com>
2017-02-26 22:28:37 -0600

    View corners

--- scripts/Game.js
+++ scripts/Game.js
@@ -8,7 +8,7 @@ const windowFirstCol = 1;
 const windowLastCol = 78;
 const sideBarLastCol = 19;
 const viewFirstCol = 22;
-const viewBottomRow = 26;
+const viewLastRow = 26;
 const titleRow = 1;
 const creditRow = 28;
 
@@ -97,12 +97,19 @@ function Game(sb) {
           game.sandbox.repeatVerticle("||", windowFirstRow, windowLastRow, sideBarLastCol+1);
         },
         displayBottomBar: function(){
-          game.sandbox.repeatHorizontal("-", viewBottomRow+1, viewFirstCol, windowLastCol);
+          game.sandbox.repeatHorizontal("-", viewLastRow+1, viewFirstCol, windowLastCol);
         },
         displayView: function(view){
-          for(var r=1; r<=viewBottomRow; r++)
+          for(var r=1; r<=viewLastRow; r++)
             if(view[r-1] !== undefined)
               game.sandbox.write(view[r-1], r, viewFirstCol);
+          game.displayViewCorners();
+        },
+        displayViewCorners: function(){
+          game.sandbox.write("/", windowFirstRow, viewFirstCol);
+          game.sandbox.write("\\", windowFirstRow, windowLastCol);
+          game.sandbox.write("\\", viewLastRow, viewFirstCol);
+          game.sandbox.write("/", viewLastRow, windowLastCol);
         },
         displayTitleBox: function(title){
           game.sandbox.repeatHorizontal("_", titleRow+1, windowFirstRow, sideBarLastCol);
