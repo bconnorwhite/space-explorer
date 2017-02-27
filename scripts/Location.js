1a2d06849617ea6e2885635008b282db41e9dc15
ConnorWhite <connor.bcw@gmail.com>
2017-02-26 22:54:06 -0600

    Beautify

--- scripts/Location.js
+++ scripts/Location.js
@@ -2,22 +2,24 @@
  *
  */
 
- function Location(gb){
-   var location = {
-     gamebox: gb,
-     init: function(){
-       location.gamebox.displaySideBar();
-       location.gamebox.displayBottomBar();
-       location.gamebox.displayView();
-       location.gamebox.displayTitleBox("SPACE EXPLORER");
-       location.gamebox.displayCreditBox(location.gamebox.explorer.credits);
-       location.gamebox.displayStatus([ location.gamebox.explorer.location.body.parent.type.toUpperCase() + ":",
-                                    " " + location.gamebox.explorer.location.body.parent.name.toUpperCase(),
-                                    location.gamebox.explorer.location.body.type.toUpperCase() + ":",
-                                    " " + location.gamebox.explorer.location.body.name.toUpperCase(),
-                                    "LOCATION:",
-                                    " " + location.gamebox.explorer.location.type.toUpperCase()]);
-     }
-   };
-   return location;
- }
+function Location(gb) {
+    var location = {
+        gamebox: gb,
+        init: function() {
+            location.gamebox.displaySideBar();
+            location.gamebox.displayBottomBar();
+            location.gamebox.displayView();
+            location.gamebox.displayTitleBox("SPACE EXPLORER");
+            location.gamebox.displayCreditBox(location.gamebox.explorer.credits);
+            location.gamebox.displayStatus(
+            [   location.gamebox.explorer.location.body.parent.type.toUpperCase() + ":",
+                " " + location.gamebox.explorer.location.body.parent.name.toUpperCase(),
+                location.gamebox.explorer.location.body.type.toUpperCase() + ":",
+                " " + location.gamebox.explorer.location.body.name.toUpperCase(),
+                "LOCATION:",
+                " " + location.gamebox.explorer.location.type.toUpperCase()
+            ]);
+        }
+    };
+    return location;
+}
