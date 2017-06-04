bf546964e7019b399f3f470800975c190c03694a
ConnorWhite <connor.bcw@gmail.com>
2017-06-04 14:17:02 -0500

    Started basic format for Colony.js

--- scripts/Colony.js
+++ scripts/Colony.js
@@ -2,19 +2,29 @@
  *
  */
 
- function Colony() {
-     var colony = {
-         init: function(game) {
-             colony.game = game;
-             colony.format();
-         },
-         format: function(){
-           colony.game.displaySideBar();
-           colony.game.displayTitleBox(colony.game.explorer.location.colony.name);
-           //colony.game.displaySideBarIcon(colony.game.explorer.location.colony.image);
-           colony.game.displayCreditBox(colony.game.explorer.credits);
-           colony.game.displayBottomBar();
-         }
-     };
-     return colony;
- }
+function Colony() {
+ var colony = {
+   init: function(game) {
+     colony.game = game;
+     colony.format();
+     colony.displayView();
+   },
+   format: function(){
+     colony.game.displaySideBar();
+     colony.game.displayTitleBox(colony.game.explorer.location.body.name, colony.game.explorer.location.colony.name);
+     colony.game.displaySideBarIcon(colony.game.explorer.location.colony.image, 5, 6);
+     //TODO: population
+     //TODO: number of explorers
+     //TODO: date
+     colony.game.displayBackButton();
+     colony.game.displayCreditBox(colony.game.explorer.credits);
+     colony.game.displayBottomBar();
+   },
+   displayView: function(){
+     colony.game.displayViewLine("Blueprints:", 2, 2);
+     //TODO: scroller with buildling blueprints
+     //TODO: scroller with rocket part blueprints OR maybe 3 scrollers, one for each type of rocket part
+   }
+ };
+ return colony;
+}
