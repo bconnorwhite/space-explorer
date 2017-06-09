56355af9426d0960264b6a5a2df2c529c3711e83
ConnorWhite <connor.bcw@gmail.com>
2017-06-08 20:44:49 -0500

    Updated Factory Layout

--- scripts/Factory.js
+++ scripts/Factory.js
@@ -12,7 +12,9 @@
      },
      format: function(){
        factory.game.displaySideBar();
-       factory.game.displayTitleBox(factory.game.explorer.location.factory.name);
+       factory.game.displayTitleBox(factory.game.explorer.location.factory.name, factory.game.explorer.location.factory.image);
+       factory.game.displayUpgrade(factory.game.explorer.location.factory.blueprint);
+       factory.game.displayBackButton();
        factory.game.displayCreditBox(factory.game.explorer.credits);
        factory.game.displayBottomBar();
      }
